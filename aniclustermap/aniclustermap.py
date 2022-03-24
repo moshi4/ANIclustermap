30c3bd8c48be2d5ff0bca373951812a96668582b
moshi <sy.41211@gmail.com>
2022-03-24 22:42:55 +0900

    Add: basic command line interface

---
+++ aniclustermap/aniclustermap.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import argparse
+import os
+from pathlib import Path
+
+__version__ = "0.1.0"
+
+
+def main():
+    """ANIclustermap main function for entrypoint"""
+    # Get argument values
+    args = get_args()
+
+    indir: Path = args.indir
+    outdir: Path = args.outdir
+    thread_num: int = args.thread_num
+
+    run(indir, outdir, thread_num)
+
+
+def run(indir: Path, outdir: Path, thread_num: int = 1) -> None:
+    """Run ANIclustermap workflow"""
+    pass
+
+
+def get_args() -> argparse.Namespace:
+    """Get arguments
+
+    Returns:
+        argparse.Namespace: Argument values
+    """
+    description = "Draw ANI(Average Nucleotide Identity) clustermap"
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        "-i",
+        "--indir",
+        required=True,
+        type=Path,
+        help="Input genome sequences directory",
+        metavar="",
+    )
+    parser.add_argument(
+        "-o",
+        "--outdir",
+        required=True,
+        type=Path,
+        help="Output directory",
+        metavar="",
+    )
+    cpu_num = os.cpu_count()
+    default_thread_num = 1 if cpu_num is None or cpu_num == 1 else cpu_num - 1
+    parser.add_argument(
+        "-t",
+        "--thread_num",
+        type=int,
+        help=f"fastANI thread numter parameter (Default: {default_thread_num})",
+        default=default_thread_num,
+        metavar="",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"v{__version__}",
+        help="Print version information",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
